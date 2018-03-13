63dceb5863c67c0a8a4e2888b5936f7709d8ccb6
alishaevn <alieva.inc@gmail.com>
2018-03-12 22:41:33 -0700

    outline for post route

---
+++ routes/teamRouter.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/User.model');
+
+router.post('/', (req, res, next) => {
+
+ 
+});
