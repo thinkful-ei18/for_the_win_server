896f5f26b04f2dbdcefdceafb60675926cc3d130
alishaevn <alieva.inc@gmail.com>
2018-03-15 08:31:49 -0700

    can retreive daily stats, but only for the day prior. the current day info does not exist

--- routes/proxyRouter.js
+++ routes/proxyRouter.js
@@ -5,7 +5,8 @@ const router = express.Router();
 const fetch = require('isomorphic-fetch');
 const btoa = require('btoa');
 
-const { API_ACTIVE_PLAYERS_BASE_URL, API_PASSWORD, API_USERNAME } = require('../config');
+const { API_ACTIVE_PLAYERS_BASE_URL, API_DAILY_STATS_BASE_URL, API_PASSWORD, API_USERNAME } = require('../config');
+
 const todayString = () => {
   let today = new Date();
   let dd = today.getDate();
@@ -23,6 +24,7 @@ const todayString = () => {
 };
 
 
+/* ========== GET ALL ACTIVE PLAYERS FROM MY SPORTS FEED ========== */
 router.get('/players', (req, res, next) => {
   const today = todayString();
 
@@ -52,6 +54,43 @@ router.get('/players', (req, res, next) => {
         };
       });
       res.json(players);
+    })
+    .catch(next);
+});
+
+
+/* ========== GET DAILY STATS FROM MY SPORTS FEED ========== */
+router.get('/stats', (req, res, next) => {
+  const today = todayString();
+
+  fetch(
+    `${API_DAILY_STATS_BASE_URL}?fordate=20180314&playerstats=2PM,3PM,FTM,REB,AST,STL,BS`, {
+      headers: {
+        'Authorization': 'Basic ' + btoa(`${API_USERNAME}:${API_PASSWORD}`)
+      }
+    }
+  )
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      const stats = data.dailyplayerstats.playerstatsentry.map(obj => ({
+        firstName: obj.player.FirstName,
+        lastName: obj.player.LastName,
+        playerID: obj.player.ID,
+        twoPointers: obj.stats.Fg2PtMade['#text'],
+        threePointers: obj.stats.Fg3PtMade['#text'],
+        freeThrows: obj.stats.FtMade['#text'],
+        rebounds: obj.stats.Reb['#text'],
+        assists: obj.stats.Ast['#text'],
+        steals: obj.stats.Stl['#text'],
+        blocks: obj.stats.Blk['#text'],
+      }));
+
+      res.json(stats);
     })
     .catch(next);
 });
