decc75de0fc4551974dc08de481869d84f24d7b0
alishaevn <alieva.inc@gmail.com>
2018-03-16 12:42:06 -0700

    added error handling

--- index.js
+++ index.js
@@ -25,11 +25,33 @@ app.use(
   })
 );
 
+
+/* ===== middleware ===== */
 app.use(express.json());
 
+
+/* ===== routes ===== */
 app.use('/register', registerRouter);
 app.use('/team', teamRouter);
 app.use('/api', proxyRouter);
+
+
+/* ===== error handling ===== */
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {}
+  });
+});
+
+
 
 function runServer(port = PORT) {
   const server = app
