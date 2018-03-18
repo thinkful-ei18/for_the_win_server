af046465ae4c75f8011f6548c5951cc34807b752
alishaevn <alieva.inc@gmail.com>
2018-03-17 19:40:31 -0700

    moved user signup to the register router

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -5,14 +5,6 @@ const router = express.Router();
 
 const User = require('../models/User.model');
 
-// /* ========== CREATE A USER ========== */
-// router.post('/', (req, res, next) => {
-//   const { email, username, password, teamName } = req.body;
-
-//   User.create({ email, username, password, teamName })
-//     .then(user => res.json(user).status(201))
-//     .catch(next);
-// });
 
 /* ========== CREATE A USER ========== */
 router.post('/', (req, res, next) => {
