d7c8cf18c342493e36ab8893960d1cc105784c9b
alishaevn <alieva.inc@gmail.com>
2018-05-13 21:06:13 -0700

    able to join a league

--- routes/leagueRouter.js
+++ routes/leagueRouter.js
@@ -4,17 +4,17 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-const User = require('../models/User.model');
 const League = require('../models/League.model');
 
 const options = { session: false, failWithError: true };
 const jwtAuth = passport.authenticate('jwt', options);
 
-/* ========== RETRIEVE A LEAGUE ========== */
+/* ========== RETRIEVE ALL LEAGUES ========== */
 router.get('/', jwtAuth, (req, res, next) => {
-  // get userId
-  // search users array within each league to see if this user's id is in there
-  // if so, return that league
+
+  League.find({})
+    .then( league => res.json(league))
+    .catch(err => next(err));
   
 });
 
@@ -28,8 +28,7 @@ router.post('/add', jwtAuth, (req, res, next) => {
   League.find({name})
     .count()
     .then( number => {
-      
-      if (number.length) {
+      if (number >= 1) {
         return Promise.reject({
           code: 422,
           reason: 'ValidationError',
@@ -43,10 +42,12 @@ router.post('/add', jwtAuth, (req, res, next) => {
         users: [ userId ]
       });
     })
-    .then(league => res.status(201).location(`${req.originalUrl}/${league.id}`).json(league) )
+    .then(league => {
+      return res.status(201).location(`${req.originalUrl}/${league.id}`).json(league);
+    })
     .catch(err => {
       if (err.code === 11000) {
-        err = new Error('The league already exists');
+        err = new Error(`'${name}' already exists`);
         err.status = 400;
       }
       next(err);
@@ -57,7 +58,35 @@ router.post('/add', jwtAuth, (req, res, next) => {
 
 /* ========== JOIN A LEAGUE ========== */
 router.put('/join', jwtAuth, (req, res, next) => {
-  
+  const userId = req.user.id;
+  const { name } = req.body;
+
+  League.find({name})
+    .then(league => {
+      const verify = league[0].users.filter(player => player === userId); 
+
+      if (verify.length < 1) {
+        if(league[0].users.length <= 4) {
+          league[0].users.push(userId);
+
+          return League.findOneAndUpdate({name}, {users: league[0].users}, {returnNewDocument: true});
+        }
+        else {
+          let err = new Error('That league is full, please select or create another');
+          err.status = 400;
+
+          throw err;
+        }
+      }
+      else {
+        let err = new Error('You are already in this league!');
+        err.status = 422;
+
+        throw err;
+      }
+    })
+    .then(league => res.json(league) )
+    .catch(err =>next(err));
  
 });
 
