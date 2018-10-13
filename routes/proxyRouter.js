e5a474f8a2ca595856d4238ce1ec8d9bfb782d65
alishaevn <alieva.inc@gmail.com>
2018-10-12 21:21:55 -0700

    added missing semicolon and commented out old code

--- routes/proxyRouter.js
+++ routes/proxyRouter.js
@@ -51,7 +51,7 @@ router.get('/players', (req, res, next) => {
 
       const selectablePlayers = currentlySignedPlayers.map(obj => {
         const playerPic = obj.player.officialImageSrc;
-        const playerName = `${obj.player.firstName} ${obj.player.lastName}`
+        const playerName = `${obj.player.firstName} ${obj.player.lastName}`;
         const playerID = obj.player.id;
         const playerTeam = obj.player.currentTeam.abbreviation;
         const playerPosition = obj.player.primaryPosition;
@@ -160,118 +160,118 @@ router.get('/games', (req, res, next) => {
 
 
 /* ========== GET USER'S LEADERBOARD FROM THE API ========== */
-router.get('/league/:name', jwtAuth, (req, res, next) => {
-  const { name } = req.params;
+// router.get('/league/:name', jwtAuth, (req, res, next) => {
+//   const { name } = req.params;
 
-  const fetchCumStats = (collectionOfIds, playerIDArray) => {
-    // fetching the cumulative stats for the current (regular & playoff) season. 
+//   const fetchCumStats = (collectionOfIds, playerIDArray) => {
+//     // fetching the cumulative stats for the current (regular & playoff) season. 
    
-    let playerIds = playerIDArray.toString();
+//     let playerIds = playerIDArray.toString();
 
-    return fetch(`${API_CUMULATIVE_STATS_BASE_URL}?player=${playerIds}&playerstats=2PM,3PM,FTM,PTS,BS,AST,REB,STL`, {
-      headers: {
-        'Authorization': 'Basic ' + btoa(`${API_USERNAME}:${API_PASSWORD}`),
-        'Accept-Encoding': 'gzip'
-      }
-    })
-      .then(response => {
-        if (!response.ok) {
-          return Promise.reject({
-            status: response.status,
-            message: response.statusText});
-        }
-        return response.json();
-      })
-      .then(data => {
+//     return fetch(`${API_CUMULATIVE_STATS_BASE_URL}?player=${playerIds}&playerstats=2PM,3PM,FTM,PTS,BS,AST,REB,STL`, {
+//       headers: {
+//         'Authorization': 'Basic ' + btoa(`${API_USERNAME}:${API_PASSWORD}`),
+//         'Accept-Encoding': 'gzip'
+//       }
+//     })
+//       .then(response => {
+//         if (!response.ok) {
+//           return Promise.reject({
+//             status: response.status,
+//             message: response.statusText});
+//         }
+//         return response.json();
+//       })
+//       .then(data => {
         
-        let arrayofStats = data.cumulativeplayerstats.playerstatsentry.map( obj => ({
-          playerId: obj.player.ID,
-          twoPointers: obj.stats.Fg2PtMade['#text'],
-          threePointers: obj.stats.Fg3PtMade['#text'],
-          freeThrows: obj.stats.FtMade['#text'],
-          totalPoints: obj.stats.Pts['#text'],
-          rebounds: obj.stats.Reb['#text'],
-          assists: obj.stats.Ast['#text'],
-          steals: obj.stats.Stl['#text'],
-          blocks: obj.stats.Blk['#text']
-        })
-        );
+//         let arrayofStats = data.cumulativeplayerstats.playerstatsentry.map( obj => ({
+//           playerId: obj.player.ID,
+//           twoPointers: obj.stats.Fg2PtMade['#text'],
+//           threePointers: obj.stats.Fg3PtMade['#text'],
+//           freeThrows: obj.stats.FtMade['#text'],
+//           totalPoints: obj.stats.Pts['#text'],
+//           rebounds: obj.stats.Reb['#text'],
+//           assists: obj.stats.Ast['#text'],
+//           steals: obj.stats.Stl['#text'],
+//           blocks: obj.stats.Blk['#text']
+//         })
+//         );
         
-        return arrayofStats;
-      })
-      .then( arrayofStats => collectionOfIds.map( array => 
-        array.map( id => 
-          arrayofStats.find(obj => 
-            obj.playerId === id)))
-      )
-      .catch(err => next(err));
-  };
+//         return arrayofStats;
+//       })
+//       .then( arrayofStats => collectionOfIds.map( array => 
+//         array.map( id => 
+//           arrayofStats.find(obj => 
+//             obj.playerId === id)))
+//       )
+//       .catch(err => next(err));
+//   };
 
   
-  let users = [];
+//   let users = [];
 
-  League.find({ name })
-    .then(league => {
-      let rivals = league[0].users.map(user => {
+//   League.find({ name })
+//     .then(league => {
+//       let rivals = league[0].users.map(user => {
 
-        users.push({ [user.username]: 0 });
-        return user.userId;
-      }); 
+//         users.push({ [user.username]: 0 });
+//         return user.userId;
+//       }); 
 
-      return Promise.all( rivals.map( id => 
-        User.find({ _id: id})
-          .then(user => user)
-      ));
-    })
-    .then(users => users.map( user => user[0].team))
-    .then(teams => 
-      teams.map(team => 
-        team.map( player => 
-          player.playerID)
-      ))
-    .then(collectionOfIds => {
+//       return Promise.all( rivals.map( id => 
+//         User.find({ _id: id})
+//           .then(user => user)
+//       ));
+//     })
+//     .then(users => users.map( user => user[0].team))
+//     .then(teams => 
+//       teams.map(team => 
+//         team.map( player => 
+//           player.playerID)
+//       ))
+//     .then(collectionOfIds => {
 
-      let allPlayerIds = [];
-      collectionOfIds.map( playerIds => 
-        playerIds.map( id =>
-          allPlayerIds.push(id)
-        ));
+//       let allPlayerIds = [];
+//       collectionOfIds.map( playerIds => 
+//         playerIds.map( id =>
+//           allPlayerIds.push(id)
+//         ));
 
-      return fetchCumStats(collectionOfIds, allPlayerIds);
-    })
-    .then(collectionOfStats => collectionOfStats.map(doc => 
-      doc.map( stat => 
-        parseInt(stat.twoPointers, 10) 
-        + parseInt(stat.threePointers, 10) 
-        + parseInt(stat.freeThrows, 10) 
-        + parseInt(stat.assists, 10) 
-        + parseInt(stat.rebounds, 10) 
-        + parseInt(stat.steals, 10) 
-        + parseInt(stat.blocks, 10)
-      )))
-    .then(playerStatTotals =>
-      playerStatTotals.map(team => {
-        let score = 0;
-        team.map(player => 
-          score += player);
+//       return fetchCumStats(collectionOfIds, allPlayerIds);
+//     })
+//     .then(collectionOfStats => collectionOfStats.map(doc => 
+//       doc.map( stat => 
+//         parseInt(stat.twoPointers, 10) 
+//         + parseInt(stat.threePointers, 10) 
+//         + parseInt(stat.freeThrows, 10) 
+//         + parseInt(stat.assists, 10) 
+//         + parseInt(stat.rebounds, 10) 
+//         + parseInt(stat.steals, 10) 
+//         + parseInt(stat.blocks, 10)
+//       )))
+//     .then(playerStatTotals =>
+//       playerStatTotals.map(team => {
+//         let score = 0;
+//         team.map(player => 
+//           score += player);
 
-        return score;
-      })
-    )
-    .then(score => {
+//         return score;
+//       })
+//     )
+//     .then(score => {
  
-      const leaderboard = users.map((user, index) => {
-        let key = Object.keys(user);
-        user[key[0]] = score[index];
+//       const leaderboard = users.map((user, index) => {
+//         let key = Object.keys(user);
+//         user[key[0]] = score[index];
 
-        return user;
-      });
+//         return user;
+//       });
 
-      res.status(200).json(leaderboard);
-    })
-    .catch(err => next(err));
+//       res.status(200).json(leaderboard);
+//     })
+//     .catch(err => next(err));
 
-});
+// });
 
 
 module.exports = router;
